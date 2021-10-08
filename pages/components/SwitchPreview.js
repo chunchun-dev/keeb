c03a27e72c87d29be06c1b66a9f507f16dae675a
qt-coder <chuchenkhang@gmail.com>
2021-10-08 14:55:01 +0800

    Small hover interaction

--- pages/components/SwitchPreview.js
+++ pages/components/SwitchPreview.js
@@ -2,12 +2,13 @@ import React from 'react'
 import Link from 'next/link'
 import { Flex } from '@chakra-ui/layout'
 import { Text } from "@chakra-ui/react"
+import { motion } from 'framer-motion'
 
 function SwitchPreview({title, type, slug, image}) {
     return (
         <Link href={`/switch/${slug}`}>
             <Flex flexDir={'column'} textAlign={'center'} maxWidth={350} minWidth={200} justifyContent={'center'} alignItems={'center'} backgroundColor={'white'} borderRadius={'lg'} p={10} marginBottom={10}>
-                <img src={image.url} width='200'/>
+                <motion.img src={image.url} width='200' whileHover={{ y: -10 }}/>
                 <Text fontSize={'lg'} fontWeight={'bold'}>{title}</Text>
                 <p>{type}</p>
             </Flex>
