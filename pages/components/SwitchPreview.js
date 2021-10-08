5405212a4cd67ab5b9f2e175d80dd7b06e4033bc
qt-coder <chuchenkhang@gmail.com>
2021-10-08 12:13:04 +0800

    Basic fetching

---
+++ pages/components/SwitchPreview.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function SwitchPreview({title, type, slug}) {
+    return (
+        <div>
+            <h1>{title}</h1>
+            <p>{type}</p>
+        </div>
+    )
+}
+
+export default SwitchPreview
