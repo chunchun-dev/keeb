7b59f537be71cd67893eb5881b50c9eec17c608c
qt-coder <chuchenkhang@gmail.com>
2021-10-08 20:29:07 +0800

    Data fetching for pages done

--- pages/switch/[slug].js
+++ pages/switch/[slug].js
@@ -1,11 +1,47 @@
 import React from 'react'
+import { gql, GraphQLClient } from 'graphql-request'
 
-function Switch() {
+function Switch({data}) {
     return (
         <div>
-            
         </div>
     )
 }
 
 export default Switch
+
+
+export const getServerSideProps = async (context) => {
+    const url = `https://api-ap-northeast-1.graphcms.com/v2/${process.env.API_ID}/master`
+    const graphQlClient = new GraphQLClient(url)
+    const pageSlug = context.query.slug
+  
+    const query = gql`
+      query($pageSlug: String!) {
+        switch(where: {
+            slug:$pageSlug
+        }) {
+          id
+          title
+          description
+          slug
+          type
+          image {
+            url
+          }
+        }
+      }
+    `
+
+    const variables = {
+        pageSlug
+    }
+  
+    const data = await graphQlClient.request(query, variables)
+  
+    return {
+        props: {
+            data
+        }
+    }
+}
